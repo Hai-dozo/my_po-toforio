f2246ea9ce04365b3e32129a4969a8920fe58594
Hai-dozo <risaishide@gmail.com>
2020-02-11 16:37:23 +0900

    Updates

---
+++ precache-manifest.02c19759b07d5bd3d29d57a1e4054ded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8794a76248174c46dd3244ff6452e017",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/index.html"
+  },
+  {
+    "revision": "fcb9d0765e0472c1b91d",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/css/main.25aee7a4.chunk.css"
+  },
+  {
+    "revision": "c382897ac3ce8e492279",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/js/2.000ec7b0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/js/2.000ec7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb9d0765e0472c1b91d",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/js/main.10bd234e.chunk.js"
+  },
+  {
+    "revision": "01561ae316f43f05bc4c",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/js/runtime-main.dec778f1.js"
+  },
+  {
+    "revision": "10a15d9b9645d698094ee9851b9f1236",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/media/china-glaze.10a15d9b.png"
+  },
+  {
+    "revision": "adcca397aa9212c1c505fd3237fd8a46",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/media/ikebana.adcca397.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "http;//hai-dozo.github.io/my_po-toforio/static/media/logo.5d5d9eef.svg"
+  }
+]);
